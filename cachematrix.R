1fb19096f118480b45f1b4c6e68c137ed752f340
Mrudul P <mrudul@metawatch.com>
2014-12-18 22:35:24 +0200

    Code for Getting Inverse of Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,25 @@
 
 ## Write a short comment describing this function
 
+# Function to cache inverse of Matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL # creating a variable m in global environment
+	set <- function(y){ # This function is used to set matrix direction
+		message("Setting y to x")
+		# assigning value to x created in parent environment. Without super assignment it would create local variable x
+		x <<- y
+		m <<- NULL
+		}
+	get <- function() { # Get Value of x
+		x
+		}
+	setInv <- function(inv) {# Store Inverse to cache
+		m <<- inv
+		}
+	getInv <- function(){
+		m
+		}
+	list (set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
@@ -12,4 +29,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getInv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        message(class(data))
+        m <- solve(data)
+        message(m)
+        x$setInv(m)
+        m
 }
